abd8f0b44f7e789f69979852fa80bba27f8d93f3
Cesar Valdez <cvj9915@gmail.com>
2020-01-01 16:06:25 -0600

    Final changes added

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,19 @@ let icons = document.getElementsByClassName('error-icon');
 btnClaim.addEventListener('click', (event) => {
     console.log(inputs);
     for (let i = 0; i < inputs.length; i++) {
+
         inputs[i].oninvalid = function(e) {
             e.target.setCustomValidity('');
             if (!e.target.validity.valid) {
-                messageErrors[i].style.display = 'block';
+                e.preventDefault();
                 icons[i].style.visibility = 'visible';
+                messageErrors[i].style.display = 'block';
+                if (i == 2) { //The email input
+                    inputs[i].placeholder = 'email@example/com';
+                    inputs[i].classList.add('email-error');
+                } else {
+                    inputs[i].placeholder = '';
+                }
             }
         }
         inputs[i].oninput = function(e) {
